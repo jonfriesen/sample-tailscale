9be84f1cff3d8a67dc82881a2c808c79715af410
Jon Friesen <jfriesen@digitalocean.com>
2021-08-10 13:00:40 -0700

    fixin' da bugs

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=app-platform-app
 do
-    # sleep 0.1
-    hello
+    sleep 0.1
 done
 echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/ /app/my-app
+ALL_PROXY=socks5://localhost:1055/ hello
